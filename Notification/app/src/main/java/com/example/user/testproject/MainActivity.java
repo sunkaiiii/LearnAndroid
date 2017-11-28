2d6655ddd857ffb25418bc9ba7addc940f2836e3
孙楷 <704725181@qq.com>
2017-11-28 16:28:23 +0800

    添加了进度通知
    添加了通知进度条
    完成后的浮动弹窗

--- Notification/app/src/main/java/com/example/user/testproject/MainActivity.java
+++ Notification/app/src/main/java/com/example/user/testproject/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,19 +22,24 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnTest,btnCancel,btnCancelOne;
+    private Button btnTest,btnCancel,btnCancelOne,btnProgress;
     EditText idEditText;
     NotificationManager notificationManager;
     int count=0;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private static final String TAG="MainActivity";
+    private void init(){
         btnTest = (Button) findViewById(R.id.btn_test_btn);
         btnCancel=(Button)findViewById(R.id.btn_cancel);
         btnCancelOne=(Button)findViewById(R.id.btn_cancel_one);
         idEditText=(EditText)findViewById(R.id.editText_id);
+        btnProgress=(Button)findViewById(R.id.btn_progress);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        init();
         btnTest.setOnClickListener((view) -> {
             Random random=new Random();
             count++;
@@ -49,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
             int id=Integer.parseInt(idEditText.getText().toString());
             cancelNotification(id);
         }));
+
+        btnProgress.setOnClickListener((view -> {
+            showProgressNotification();
+        }));
     }
 
     private void cancelNotification(int id){
@@ -59,7 +69,34 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.cancelAll();
         }
     }
+    private void showProgressNotification(){
+        notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"progress");
+        builder.setContentTitle("Download")
+                .setContentText("download in progress")
+                .setSmallIcon(R.drawable.choclate);
+        //启动一个虚假的下载线程
+        new Thread(()->{
+            int incr;
 
+            for(incr=0;incr<=100;incr+=5){
+                builder.setProgress(100,incr,false);
+                notificationManager.notify(1,builder.build());
+                try{
+                    Thread.sleep(1*100);
+                }catch (InterruptedException e){
+                    Log.d(TAG,"sleep error");
+                }
+            }
+            //循环完成，更新notification，然后销毁
+            builder.setContentText("Download complete").setProgress(0,0,false);
+
+            //设置优先级并使用响铃和震动，用于触发Android 5.1以上的浮动通知
+            builder.setPriority(NotificationCompat.PRIORITY_MAX).setDefaults(NotificationCompat.DEFAULT_ALL);
+            notificationManager.notify(1,builder.build());
+        }).start();
+
+    }
     private void sendNotification(int id,String content) {
         Intent mainIntent = new Intent(this, MainActivity.class);
 
@@ -67,11 +104,11 @@ public class MainActivity extends AppCompatActivity {
         //在这里应用为用户点击通知，才会执行
         PendingIntent mainPendingIntent=PendingIntent.getActivity(this,1,mainIntent,PendingIntent.FLAG_UPDATE_CURRENT);
         Resources res=getResources();
-        Bitmap bit=BitmapFactory.decodeResource(res,R.drawable.ic_launcher_background);
+        Bitmap bit=BitmapFactory.decodeResource(res,R.drawable.choclate);
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         if(Build.VERSION.SDK_INT>=26) {
             Notification builder = new Notification.Builder(this, "tag")
-                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setSmallIcon(R.drawable.choclate)
                     .setLargeIcon(bit)
                     .setContentTitle(content)
                     .setContentText(content + content + content + content)
@@ -82,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "tag")
-                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setSmallIcon(R.drawable.choclate)
                     .setLargeIcon(bit)
                     .setContentTitle(content)
                     .setContentText(content + content + content + content)
